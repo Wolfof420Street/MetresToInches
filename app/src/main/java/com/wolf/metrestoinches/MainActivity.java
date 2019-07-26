533890ce47919765046fa0973bc3e757ba013920
Wolfof420Street <billodida420@gmail.com>
2019-07-26 12:38:38 +0300

    deploying simple app

---
+++ app/src/main/java/com/wolf/metrestoinches/MainActivity.java
@@ -0,0 +1,39 @@
+package com.wolf.metrestoinches;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText enterMetres;
+    private Button convertButton;
+    private TextView resultTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        enterMetres = (EditText) findViewById(R.id.metresEditText);
+        convertButton = (Button) findViewById(R.id.convertButtonId);
+        resultTextView = (TextView) findViewById(R.id.resultId);
+
+        convertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double multiplier = 39.37;
+                double result = 0.0;
+
+                double meterValue = Double.parseDouble(enterMetres.getText().toString());
+                result = meterValue*multiplier;
+
+                resultTextView.setText(Double.toString(result));
+            }
+        });
+    }
+}
